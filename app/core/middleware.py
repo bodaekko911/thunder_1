085f59cedc5b7266bbc80abfa4daff3eaaab8060
bodaekko911 <bodaekko@gmail.com>
2026-04-24 22:29:01 +0300

    s

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -96,7 +96,7 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers.setdefault(
             "Content-Security-Policy",
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline'; "
+            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "font-src 'self' https://fonts.gstatic.com; "
             "img-src 'self' data:; "
